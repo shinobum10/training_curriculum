d2c0319694a818825b4d2fb2275ddf91f63e4a7f
shinobum10 <snb025562@gmail.com>
2021-11-02 12:37:17 +0900

    Issue1~3の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -37,7 +37,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      days = { month: '@todays_date + x.month’, date: '@todays_date + x.day’, plans: 'today_plan'}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
 
       @week_days.push(days)
     end
